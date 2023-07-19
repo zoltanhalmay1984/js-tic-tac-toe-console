d2f6245907ba0cb372aaedaccebf016bc84b08c5
Zoltán Halmay <132577193+zoltanhalmay1984@users.noreply.github.com>
2023-07-19 11:03:32 +0200

    playing in sandbox

--- tictactoe.js
+++ tictactoe.js
@@ -9,7 +9,11 @@ console.log(playerName)
 */
 
 function getEmptyBoard() {
-    return [[".", ".", "."], [".", ".", "."], [".", ".", "."]];
+    return [
+        [".", ".", "."],
+        [".", ".", "."],
+        [".", ".", "."]
+    ];
 }
 
 //console.log(getEmptyBoard());
@@ -18,31 +22,31 @@ function getHumanCoordinates() {
     const playerInput = (prompt('Type your coordinates, then press enter: '));
     switch (playerInput.toUpperCase()) {
         case "A1":
-            return [0,0];
+            return [0, 0];
             break;
         case "A2":
-            return [0,1];
+            return [0, 1];
             break;
         case "A3":
-            return [0,2];
+            return [0, 2];
             break;
         case "B1":
-            return [1,0];
+            return [1, 0];
             break;
         case "B2":
-            return [1,1];
+            return [1, 1];
             break;
         case "B3":
-            return [1,2];
+            return [1, 2];
             break;
         case "C1":
-            return [2,0];
+            return [2, 0];
             break;
         case "C2":
-            return [2,1];
+            return [2, 1];
             break;
         case "C3":
-            return [2,2];
+            return [2, 2];
             break;
         default:
             return getHumanCoordinates();
@@ -53,7 +57,7 @@ function movesHistory(moveTaken) {
     return slotsOccupied.add(moveTaken);
 }
 
-function visualizeMove(moveTaken, board) {
+function visualizeMove(moveTaken) {
     board[moveTaken[0]][(moveTaken[1])] = "X";
 }
 
@@ -63,7 +67,10 @@ do {
     board = getEmptyBoard();
     moveTaken = getHumanCoordinates();
     movesHistory(moveTaken);
+
     visualizeMove(moveTaken, board)
+
+    //    console.log(board[0] + "\n" + board[1] + "\n" + board[2]);
     console.log(board)
 } while (true)
 
